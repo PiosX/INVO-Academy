7701a06b210b375ee52d51d5f4b9f034314110bd
PiosX <piosso.pios@op.pl>
2023-04-19 17:23:02 +0200

    Spotify - Order Summary Panel

--- Spotify/Order-Summary-Panel/src/pages/home-page/home-page.js
+++ Spotify/Order-Summary-Panel/src/pages/home-page/home-page.js
@@ -5,8 +5,19 @@ import "./home-page.scss";
 
 const button = document.querySelector(".order__primary-btn");
 
+let isLoading = false;
+
 const clickHandler = () => {
+	if (isLoading) {
+		console.log("The payment is already processing");
+		return;
+	}
 	button.setAttribute("data-loading", "1");
+	button.setAttribute("disabled", "disabled");
+
+	isLoading = true;
+
+	return isLoading;
 };
 
 button.addEventListener("click", clickHandler);
